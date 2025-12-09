f562880211a6c1094f73678634fe19b5c73d6750
nineeko <gkxm1013@naver.com>
2025-12-09 13:49:18 +0900

    CHORE: 초기 설정

--- src/test/java/com/tickatch/logservice/ProjectInterfaceApplicationTests.java
+++ src/test/java/com/tickatch/logservice/ProjectInterfaceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tickatch.projectinterface;
+package com.tickatch.logservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectInterfaceApplicationTests {
+class logserviceApplicationTests {
 
   @Test
   void contextLoads() {}
