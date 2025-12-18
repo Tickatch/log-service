ef2104d0e353d325ab584e8633e46adec65868a2
nineeko <gkxm1013@naver.com>
2025-12-18 10:47:36 +0900

    REFACTOR: 이전 버전 로그 등록 파일 삭제
    See also: #26

--- src/main/java/com/tickatch/logservice/log/domain/repository/EventLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tickatch.logservice.log.domain.repository;
-
-import com.tickatch.logservice.log.domain.EventLog;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventLogRepository extends JpaRepository<EventLog, UUID> {}
